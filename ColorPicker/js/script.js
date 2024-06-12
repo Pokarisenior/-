63b7f804e2577d25323f9eb78ef733173beffaf9
Pokarisenior <hansan1015@gmail.com>
2024-06-12 17:19:37 +0900

    Update script.js

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -1,10 +1,9 @@
 const text = document.querySelector('#colorText');
 const color= document.querySelector('#colorPicker');
-
+//カラーピッカーが変更されたら、カラーコードを表示
 const colorBg=()=>{
   text.textContent = `カラーコード：${color.value}`;
-  
-
+};
 color.addEventListener('input',colorBg);
 
 
