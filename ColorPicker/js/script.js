7245062661f9328535e1d56ec14d4861ccc64c2b
Pokarisenior <hansan1015@gmail.com>
2024-06-12 18:06:04 +0900

    Update script.js

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -3,8 +3,13 @@ const color= document.querySelector('#colorPicker');
 //カラーピッカーが変更されたら、カラーコードを表示
 const colorBg=()=>{
   document.body.style.backgroundColor=color.value;
+  if(color.value=== '#ffffff'){
+    text.textContent=`カラーコード：${color.value}(white)`;
+  }else if(color.value==='#000000`){
+    text.textContent=`カラーコード：${color.value}(black)`;
+  }else{
   text.textContent = `カラーコード：${color.value}`;
-};
+  };
 color.addEventListener('input',colorBg);
 
 
