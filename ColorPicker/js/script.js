a15bb718527af460288040b2edc30edc2e25e3f7
Pokarisenior <hansan1015@gmail.com>
2024-06-12 16:46:48 +0900

    Update script.js

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -1,1 +1,1 @@
-document.querySelector('#colorText').textContent = document.querySelector('#colorPicker').value;
+document.querySelector('#colorText').textContent = `カラーコード：${document.querySelector('#colorPicker').value}`;
